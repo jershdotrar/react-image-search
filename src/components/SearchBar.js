961f67adbc49f1fc8cdaa13aa7b0f6f03e1c33ff
jersh <zombine.rar@gmail.com>
2020-02-11 23:35:30 -0600

    updates search bar to use state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onFormSubmit = ( e ) => {
+        e.preventDefault();
+        this.props.onSubmit( this.state.term );
+    }
+
     render() {
         return (
-            <div>Search Bar</div>
+            <div className="ui segment">
+                <form onSubmit={ this.onFormSubmit } className="ui form">
+                    <div className="field">
+                        <label>Image search</label>
+                        <input
+                            type="text"
+                            value={ this.state.term }
+                            onChange={ (e) => this.setState({ term: e.target.value }) }
+                        />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
